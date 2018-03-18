944f822c68f5250785c4023284209358082e9cb0
Koen De Voegt <koen@devoegt.be>
2018-03-18 12:04:54 +0100

    test cleanup

--- src/test/java/com/nanoxic/nanorpc4j/tests/WalletTest.java
+++ src/test/java/com/nanoxic/nanorpc4j/tests/WalletTest.java
@@ -1,7 +1,5 @@
 package com.nanoxic.nanorpc4j.tests;
 
-import java.util.List;
-
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.junit.BeforeClass;
@@ -23,7 +21,7 @@ public class WalletTest {
 		String hostname = config.getString("hostname");
 		int port = config.getInt("port");
 
-		walletId = config.getString("wallet");
+		walletId = config.getString("walletId");
 
 		// initialize the client
 		Node.init(hostname, port);
@@ -32,33 +30,37 @@ public class WalletTest {
 	@Test
 	public void validWalletIdTest() {
 		System.out.println(walletId);
-		Wallet wallet = new Wallet(walletId);
-		List<String> i = wallet.getAllAddresses();
-		for (String address : i) {
-			System.out.println(address);
-		}
+		// Wallet wallet =
+		new Wallet(walletId);
+		// List<String> i = wallet.getAllAddresses();
+		// for (String address : i) {
+		// System.out.println(address);
+		// }
 	}
 
-//	@Test
-//	public void invalidWalletIdTest() {
-//		System.out.println(walletId);
-//		Wallet wallet = new Wallet("XXXXXX");
-//		List<String> i = wallet.getAllAddresses();
-//		System.out.println(i);
-//		System.out.println(wallet.isLocked());
-//		System.out.println(wallet.getBalance());
-//		for (String address : i) {
-//			System.out.println(address);
-//		}
-//	}
+	// @Test
+	// public void invalidWalletIdTest() {
+	// System.out.println(walletId);
+	// Wallet wallet = new Wallet("XXXXXX");
+	// List<String> i = wallet.getAllAddresses();
+	// System.out.println(i);
+	// System.out.println(wallet.isLocked());
+	// System.out.println(wallet.getBalance());
+	// for (String address : i) {
+	// System.out.println(address);
+	// }
+	// }
 
-//	@Test
-//	public void Test() {
-//		Account account = new Account("xrb_39afy3dbjdoeskk1x84imwrp59ubn9ghafyzpt67t7inyujn5xs1t5xptueq");
-//		Wallet wallet = new Wallet(walletId);
-//
-//		account.getBalance();
-//		assertEquals(new BigInteger("5191855022866341132517769216"), account.getBalance());
-//		assertEquals(new BigInteger("5191855022866341132517769216"), wallet.getBalance());
-//	}
+	// @Test
+	// public void Test() {
+	// Account account = new
+	// Account("xrb_39afy3dbjdoeskk1x84imwrp59ubn9ghafyzpt67t7inyujn5xs1t5xptueq");
+	// Wallet wallet = new Wallet(walletId);
+	//
+	// account.getBalance();
+	// assertEquals(new BigInteger("5191855022866341132517769216"),
+	// account.getBalance());
+	// assertEquals(new BigInteger("5191855022866341132517769216"),
+	// wallet.getBalance());
+	// }
 }
